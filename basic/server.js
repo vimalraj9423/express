8dc3444f44131e41cc3a88b7c6d93eedc0f9bfcf
vimalraj9423 <vimalraj9423@gmail.com>
2019-02-28 17:37:58 +0530

    changes for swagger

--- basic/server.js
+++ basic/server.js
@@ -2,18 +2,21 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors"; 
 import morgan from 'morgan';
-
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
 const port = 3000;  
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
 //Index route
+app.get('/users', (req, res) => {
+    res.send('users');
+});
 app.get('/', (req, res) => {
     res.send('Invalid endpoint!');
 });
-
 
 app.listen(port, () => {
     console.log('server started - ', port);
